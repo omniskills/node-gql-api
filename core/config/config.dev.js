3ee8510ce37642dc7e57d2dbd1485c1d91686643
omniskills <14161165+omniskills@users.noreply.github.com>
2018-04-30 17:23:52 +0800

    Change port config

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -12,7 +12,7 @@ config.dbPort = process.env.dbPort || '27017';
 config.dbName = process.env.dbName || 'trains';
 config.dbUser = process.env.dbUser || 'root';
 config.dbPassword = process.env.dbPassword || 'password';
-config.serverPort = process.env.serverPort || 8000;
+config.serverPort = process.env.PORT || 8000;
 config.SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR || 10;
 config.JWT_SECRET = process.env.JWT_SECRET || 'JWT_SECRET';
 
